2922b53047703af4d7ecf8c7fcc3f8fac592595b
bwk5140 <133719936+bwk5140@users.noreply.github.com>
2024-12-12 20:54:27 +0300

    Updated references to FrozenLakeAgent to Agent

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@
 #Pennsylvania State University, Course : SWENG480
 #Authors: Katie Killian, Brian Wachira, and Nicole Vadillo
 
-from src.Memphis.ccrg.LIDA.Framework.Agents.Agent import FrozenLakeAgent
+from src.Memphis.ccrg.LIDA.Framework.Agents.Agent import Agent
 
 if __name__ == "__main__":
-    agent = FrozenLakeAgent() #instantiate FrozenLakeAgent / create instance of the class
+    agent = Agent() #instantiate FrozenLakeAgent / create instance of the class
     agent.run()
