76b0f9dc5407d931fbbacb3e957038b5cc75328b
kkillian0261 <ksk5472@psu.edu>
2024-12-15 17:31:33 -0500

    Updated PyTesting UnitTesting

--- PyTesting/UnitTesting/Environment Testing/test_environment.py
+++ PyTesting/UnitTesting/Environment Testing/test_environment.py
@@ -1,15 +1,37 @@
 import pytest
 from src.Memphis.ccrg.LIDA.Environment.Environment import FrozenLakeEnvironment
 
-def test_reset():
-    #Generating an instance of FrozenLakeEnvironment
-    env = FrozenLakeEnvironment()
+#Defining a fixture function
+#initializing resources
+@pytest.fixture
+def environment():
+    return FrozenLakeEnvironment(render_mode="human")
 
+def test_reset(environment):
     #Calling the reset
-    state, info, col, row = env.reset()
-
+    state, info, col, row = environment.reset()
     #Asserting initial conditions
-    assert isinstance(state, (int, tuple))
+    assert state is not None #State should not be none after reset
+    assert info is not None #Info should be a dictionary
+    assert col == 0 #Column should be reset to 0
+    assert row == 0 #Row should be reset to 0
+
+def test_step(environment):
+    environment.reset() #ensuring environment is reset
+    action = environment.action_space.sample() #random action
+    new_state, reward, done, truncated, info = environment.step(action)
+    #Assertions
+    assert new_state is not None
+    assert info is not None
+    assert isinstance(reward, (int,float))
+    assert isinstance(done, bool)
+    assert isinstance(truncated, bool)
     assert isinstance(info, dict)
-    assert col == 0
-    assert row == 0
+
+def test_render(environment):
+    environment.reset()
+    try:
+        environment.render()
+    except Exception as e:
+        pytest.fail(f"Rendering failed: {e}")
+
