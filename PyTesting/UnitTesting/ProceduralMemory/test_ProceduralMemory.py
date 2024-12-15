478de974782af0be0a7dd3dc545655624032f9ff
kkillian0261 <ksk5472@psu.edu>
2024-12-15 18:27:59 -0500

    Updated Procedural Memory Testing

---
+++ PyTesting/UnitTesting/ProceduralMemory/test_ProceduralMemory.py
@@ -0,0 +1,16 @@
+import pytest
+from src.Memphis.ccrg.LIDA.ProceduralMemory.ProceduralMemory import ProceduralMemory
+
+def test_add_scheme():
+    procedural_memory = ProceduralMemory()
+    procedural_memory.add_scheme("goal", "move_forward")
+    assert procedural_memory.schemes["goal"] == "move_forward"
+
+def test_get_action_existing():
+    procedural_memory = ProceduralMemory()
+    procedural_memory.add_scheme("goal", "move_forward")
+    assert procedural_memory.get_action("goal") == "move_forward"
+
+def test_get_action_not_existing():
+    procedural_memory = ProceduralMemory()
+    assert procedural_memory.get_action("unknown") is None
