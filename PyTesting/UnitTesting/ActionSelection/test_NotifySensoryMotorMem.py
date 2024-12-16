58eecbd3ebf67eb3220d3194696f1da0752ec136
bwk5140 <133719936+bwk5140@users.noreply.github.com>
2024-12-16 16:29:28 +0300

    Updated @pytest.fixture resource methods

--- PyTesting/UnitTesting/ActionSelection/test_NotifySensoryMotorMem.py
+++ PyTesting/UnitTesting/ActionSelection/test_NotifySensoryMotorMem.py
@@ -8,53 +8,48 @@ from src.ProceduralMemory.ProceduralMemory import ProceduralMemory
 
 #initializing resources
 @pytest.fixture
-def test_action_select():
+def action_select():
     return ActionSelection
 
 #initializing resources
 @pytest.fixture
-def test_sensory_motor_mem():
+def sensory_motor_mem():
     return SensoryMotorMemoryImpl
 
-#initializing resources
-@pytest.fixture
-def test_procedural_mem():
-    return ProceduralMemory
-
-def test_notify_sensory_motor_mem(test_action_select, test_sensory_motor_mem):
-    test_action_select.notify_sensory_motor_memory =(
+def test_notify_sensory_motor_mem(action_select, sensory_motor_mem):
+    action_select.notify_sensory_motor_memory =(
         MagicMock(return_value=["test_state", 0.0, True, False,
                                 {"info": "test_information"}]))
-    test_action_select.notify_sensory_motor_memory(1,
-                                                      test_sensory_motor_mem
+    action_select.notify_sensory_motor_memory(1,
+                                                      sensory_motor_mem
                                                       )
-    state, reward, done, truncated, info = (test_action_select.
+    state, reward, done, truncated, info = (action_select.
                                             notify_sensory_motor_memory(
-                                                1, test_sensory_motor_mem
+                                                1, sensory_motor_mem
                                             ))
     assert state == "test_state"
     assert reward == 0.0
     assert done == True
     assert truncated == False
     assert info == {'info': 'test_information'}
-    (test_action_select.notify_sensory_motor_memory.
-     assert_called_with(1, test_sensory_motor_mem))
+    (action_select.notify_sensory_motor_memory.
+     assert_called_with(1, sensory_motor_mem))
 
 
-def test_action_selected(test_action_select, test_sensory_motor_mem):
-    test_action_select.select_action = (
+def test_action_selected(action_select, sensory_motor_mem):
+    action_select.select_action = (
         MagicMock(return_value=["test_state", 0, 0.0, True, False,
                                 {"info": "test_information"}]))
-    test_action_select.select_action(["state-0"], 0,
-                                        test_sensory_motor_mem)
-    state, state_id, reward, done, truncated, info = (test_action_select.
+    action_select.select_action(["state-0"], 0,
+                                        sensory_motor_mem)
+    state, state_id, reward, done, truncated, info = (action_select.
                                                 select_action(
                                                 ['state-0'], 0,
-                                                test_sensory_motor_mem))
+                                                sensory_motor_mem))
     assert state == "test_state"
     assert state_id == 0
     assert reward == 0
     assert truncated == 0
     assert info == {'info': 'test_information'}
-    test_action_select.select_action.assert_called_with(["state-0"], 0
-                                        , test_sensory_motor_mem)
+    action_select.select_action.assert_called_with(["state-0"], 0
+                                        , sensory_motor_mem)
