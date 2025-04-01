764d32326f509050e6e73eabc8055ddb17d4a28f
kkillian0261 <ksk5472@psu.edu>
2025-04-01 14:19:14 -0400

    Implemented Procedural Mem Unit Test Cases

--- PyTesting/UnitTesting/ProceduralMemory/test_ProceduralMemory.py
+++ PyTesting/UnitTesting/ProceduralMemory/test_ProceduralMemory.py
@@ -1,22 +1,45 @@
-from source.ProceduralMemory.ProceduralMemory import ProceduralMemory
+import pytest
+
+from unittest.mock import Mock
+from source.ProceduralMemory.ProceduralMemoryImpl import ProceduralMemoryImpl
+from source.PAM.PAM import PerceptualAssociativeMemory
+from source.GlobalWorkspace.GlobalWorkSpace import GlobalWorkspace
+from source.Framework.Shared.NodeImpl import NodeImpl
+from source.Framework.Shared.LinkImpl import LinkImpl
 
 """
 This provided PyTest is for the Procedural Memory ModuleSubject:
 It provides tests for the specific functions: add_scheme, get_action
 As development continues these are subject to change or update as the module does. 
-Test Cases: TC-040, TC-041.
+Test Cases: 
 """
 
-def test_add_scheme():
-    procedural_memory = ProceduralMemory()
-    procedural_memory.add_scheme("goal", "move_forward")
-    assert procedural_memory.schemes["goal"] == "move_forward"
+@pytest.fixture
+def procedural_memory():
+    return ProceduralMemoryImpl()
 
-def test_get_action_existing():
-    procedural_memory = ProceduralMemory()
-    procedural_memory.add_scheme("goal", "move_forward")
-    assert procedural_memory.get_action("goal") == "move_forward"
+def test_initialization(procedural_memory):
+    assert procedural_memory.logger is not None
 
-def test_get_action_not_existing():
-    procedural_memory = ProceduralMemory()
-    assert procedural_memory.get_action("unknown") is None
+"""IN WORKING PROCESSES & ADD NOTIFY WITH GW"""
+def test_notify_with_PAM(procedural_memory):
+    #Mocking the PAM
+    module = Mock(spec=PerceptualAssociativeMemory)
+    state = Mock(spec=NodeImpl)
+    module.__getstate__.return_value = state
+    module.retrieve_association.return_value = ['assoication1','association2']
+
+    procedural_memory.notify(module)
+
+    assert procedural_memory.state == state
+
+def test_activate_Scheme(procedural_memory):
+    associations = [Mock(spec=LinkImpl), Mock(spec=NodeImpl)]
+    procedural_memory.activate_Scheme(associations)
+
+"""
+Add testing for functions:
+    Learn
+    get closest match
+    seek goal
+"""
