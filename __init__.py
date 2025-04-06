cf1301f92c6d7e2bc06bff22a96a4881ac7dd88e
bwk5140 <133719936+bwk5140@users.noreply.github.com>
2025-04-07 00:48:29 +0100

    Module updates. Conscious broadcasts to PAM and Procedural memory with learning (testing learning from broadcasts currently).

---
+++ __init__.py
@@ -0,0 +1,19 @@
+
+from source import ActionSelection
+from source import AttentionCodelets
+from source import Environment
+from source import Framework
+from source.Framework import Agents
+from source.Framework import Initialization
+from source.Framework import Shared
+from source.Framework import Strategies
+from source import GlobalWorkspace
+from source import ModuleInitialization
+from source import PAM
+from source import ProceduralMemory
+from source import SensoryMemory
+from source import SensoryMotorMemory
+from source import Workspace
+from source.Workspace import CurrentSituationModel
+from source.Workspace import StructureBuildingCodelets
+
